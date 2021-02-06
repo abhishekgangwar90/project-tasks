5436983b1c6b0792420d404080ba6c6fd981989b
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-02-07 03:33:32 +0530

    feat: login page changes

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './login.scss';
 
 function Login() {
-  return <div className="test">login page</div>;
+  return (
+    <div className="login">
+      <div className="login__card">
+        <div className="login__description">
+          <h1>TASKY</h1>
+          <h2>Welcome Back</h2>
+          <p>
+            To keep connected with us, Please login with your personal
+            information.
+          </p>
+        </div>
+        <div className="login__content">Content</div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
