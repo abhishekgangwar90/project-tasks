a5e88227f4de8dfc1a15c4bcd30d2e0d30b6e962
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-03-03 22:39:16 +0530

    feat: filter fixes

--- src/organisms/Navigation/navigation.js
+++ src/organisms/Navigation/navigation.js
@@ -8,20 +8,19 @@ import './navigation.scss';
 function Navigation() {
   return (
     <div className="navigation">
-      <div className="navigation__content">
+      <div className="navigation__button">
         <Button
           title="+ Add New Task"
           size="medium"
           color="green-dark"
           animated
-          classes="u-margin-bottom-large"
         />
-        <div className="navigation__filter u-margin-top-large u-margin-bottom-small">
-          <Filters />
-        </div>
-        <div className="navigation__taskView">
-          <TasksList />
-        </div>
+      </div>
+      <div className="navigation__filter">
+        <Filters />
+      </div>
+      <div className="navigation__taskView">
+        <TasksList />
       </div>
       <div className="navigation__footer">
         &#64; 2020 By Abhishek, All rights reserved.
